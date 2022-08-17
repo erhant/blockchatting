eb5fda0d6fefcac00eb78e8758ad815ab9691715
Erhan Tezcan <erhany96@gmail.com>
2022-08-17 14:58:37 +0300

    favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -26,6 +26,13 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <title>NextJS + Web3 Starter</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <meta name="description" content="A NextJS + Web3 starter template, supporting Hardhat with TypeScript." />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
